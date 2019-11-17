4ca8fb50829904162120324b975c9a3c0bd988e7
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-17 15:51:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,6 +30,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+  retrieve_element_from_index = ["Damian", "Osvaldo"]
+  retrieve_element_from_index[0]
   
 end
 
