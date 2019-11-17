4f940742ff95565b17bacf97f287d3d0b007e03c
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-17 15:42:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = ["Damian", "arrays!"]
   remove_element_from_end_of_array.pop
-  
+  last_one = 
 end
 
 def remove_element_from_start_of_array(array)
