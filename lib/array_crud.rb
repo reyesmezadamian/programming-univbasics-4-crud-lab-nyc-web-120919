c6a4ad2a68445fffd3370991e0fa7f40f0580063
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-17 15:30:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array = ["Damian", "Osvaldo"]
-  add_element_to_start_of_array.unshift("array!")
+  add_element_to_start_of_array.unshift("Osvaldo")
 end
 
 def remove_element_from_end_of_array(array)
