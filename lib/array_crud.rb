2c07e618d5f95299dc2820accca8b2517bfb4fb2
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-17 15:28:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array = ["Damian", "Osvaldo"]
-  
+  add_element_to_end_of_array << "Juan"
 end
 
 def add_element_to_start_of_array(array, element)
