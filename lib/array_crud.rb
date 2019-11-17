99da5d98f7da620dfffbb40fe2f17529ba163f60
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-17 15:54:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -46,5 +46,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+ update_element_from_index=["mom", "dad", "cat"]
+ update_element_from_index
 end
