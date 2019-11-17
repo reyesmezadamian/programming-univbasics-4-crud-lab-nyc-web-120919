bbc8934a587def4a3102b1b88882a8e797a8f445
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-17 15:43:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = ["Damian", "arrays!",]
-  last_one = remove_element_from_end_of_array.pop
+  last_one = "wow"
   puts last_one
   return last_one
   
